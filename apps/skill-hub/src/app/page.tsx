6a329427e19db0f122a3b3aa1d288349c5141818
jasu55 <67340668+jasu55@users.noreply.github.com>
2025-12-28 15:32:05 +0800

    feat: regex

--- apps/skill-hub/src/app/page.tsx
+++ apps/skill-hub/src/app/page.tsx
@@ -1,15 +1,17 @@
 import { Button } from '@intern-3a/shadcn';
 import { AllClubsCardScrollAnimation } from './_components';
-import Intro from './_components/Intro';
+import { Intro } from './_components/Intro';
 import Map from './_components/Map';
+import { FilteredClubs } from './_components/FilteredClubs';
 
 export default function Index() {
   return (
     <div>
       <Button variant="destructive">Hello</Button>
       <Intro />
-      <FilteredClubs />
       <AllClubsCardScrollAnimation />
+
+      <FilteredClubs />
       <Map />
     </div>
   );
