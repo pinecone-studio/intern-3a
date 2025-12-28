0dd121567e790f184f9dc0ce63f3cc464754f817
Amar Bodonguud <64552518+ramamaa@users.noreply.github.com>
2025-12-28 16:50:51 +0800

    feat(amar): new project Team Study-Plan

---
+++ apps/amar/src/app/page.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable @nx/enforce-module-boundaries */
+import { Button, Input } from '@intern-3a/shadcn';
+export default function Index() {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.tailwind file.
+   */
+  return (
+    <div>
+      <Button>Add</Button>
+      <Input placeholder="testing ....." />
+    </div>
+  );
+}
