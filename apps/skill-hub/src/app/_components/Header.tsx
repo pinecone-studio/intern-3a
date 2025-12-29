61d65cab7577aa6dcb4d1003ef4dd8a251e20ff2
oyunmyagmar <25LP4018@nest.edu.mn>
2025-12-30 06:14:23 +0900

    feat(skill-hub): club info input zassan

--- apps/skill-hub/src/app/_components/Header.tsx
+++ apps/skill-hub/src/app/_components/Header.tsx
@@ -8,8 +8,9 @@ import { MyUserButton } from './MyUserButton';
 
 export const Header = () => {
   const [open, setOpen] = useState(false);
+
   return (
-    <div className="w-full h-20 bg-blue-950 sticky top-0 z-50 flex justify-between items-center px-10">
+    <div className="w-full h-20 bg-[#0A427A] sticky top-0 z-50 flex justify-between items-center px-10">
       <div className="flex items-center h-full gap-4">
         <Image className="text-white font-bold text-xl" width={40} height={40} src="/logo.png" alt="Logo" />
         <Link href={'/'}>
@@ -24,10 +25,10 @@ export const Header = () => {
         </Link>
       </div>
 
-      <div className="flex gap-5">
+      <div className="flex gap-5 items-center">
         <Dialog open={open} onOpenChange={setOpen}>
           <DialogTrigger asChild>
-            <Button className="bg-orange-500 rounded-full cursor-pointer">Дугуйлан Бүртгүүлэх</Button>
+            <Button className="bg-[#FCB027] hover:bg-[#f5a81d] rounded-full cursor-pointer">Дугуйлан Бүртгүүлэх</Button>
           </DialogTrigger>
           <ClubRegisterBtnDialogContent setOpen={setOpen} />
         </Dialog>
--- apps/skill-hub/src/lib/utils/types.ts
+++ apps/skill-hub/src/lib/utils/types.ts
@@ -17,6 +17,19 @@ export type WeekScheduleType = Partial<Record<WeekDayType, DaytimeType>>;
 
 export type ScheduledClubTimesByClassLevelsType = Partial<Record<ClassLevelsType, WeekScheduleType>>;
 
+export type TeacherInfoType = {
+  teacherImage?: string | File;
+  teacherImagePreview?: string;
+  teacherName: string;
+  teacherPhone: string;
+  teacherEmail: string;
+  teacherProfession: string;
+  teacherExperience: string;
+  teacherAchievement: string;
+};
+
+export type TeachersByClassLevelsType = { [key in ClassLevelsType]?: TeacherInfoType };
+
 export type NewClubType = {
   _id?: string;
   clubName: string;
