b4645ddd3146040347a01e38c8b26f03bf064368
Iori205 <b.sodbilegt11@gmail.com>
2025-12-30 11:51:31 +0800

    BE (#93)

--- apps/unifind/src/app/page.tsx
+++ apps/unifind/src/app/page.tsx
@@ -1,8 +1,16 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
 export default function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.tailwind file.
-   */
-  return <div className="bg-red-200">hello</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch('/api/universities')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log({ data });
+
+  return <div className="bg-red-200">bnn</div>;
 }
