76cbc68699c6add9a366219138bcf840bf2e0d18
oyunmyagmar <oyunmyagmar.g@gmail.com>
2025-12-30 18:26:15 +0800

    feat(skill-hub): filter real data (#107)

--- apps/skill-hub/src/app/page.tsx
+++ apps/skill-hub/src/app/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { useAuth, useUser } from '@clerk/nextjs';
-import { Button, Spinner } from '@intern-3a/shadcn';
+import { Spinner } from '@intern-3a/shadcn';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import { AllClubsCardScrollAnimation, ClubFilterSection } from './_components';
-// import { FilteredClubs } from './_components/filteredClubs';
+import { AllClubsCardScrollAnimation, ClubFilterSection, FilteredClubsForUser } from './_components';
+import { FilteredClubs } from './_components/filteredClubs';
 import { Intro } from './_components/Intro';
 
 export default function Index() {
@@ -52,8 +52,8 @@ export default function Index() {
     <div>
       <Intro />
       <AllClubsCardScrollAnimation />
+      <FilteredClubsForUser />
       <ClubFilterSection />
-
       {/* <FilteredClubs /> */}
     </div>
   );
