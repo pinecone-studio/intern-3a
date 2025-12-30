b11c70ebc21444d33dd2b61980b9c66be300f227
erdenetsogta <a.erdenetsogt@gmail.com>
2025-12-30 10:55:58 +0800

    chore: add vercel

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ rm -rf ./$2/.next
 rm -rf .vercel
 mkdir .vercel
 cp -r ./$2/vercel-$1.json .vercel/project.json
-vercel env pull .env.production.local
+yarn vercel env pull .env.production.local
 mv .env.production.local ./$2/
 nx reset
-vercel build --prod
-vercel --prebuilt --prod
+yarn vercel build --prod
+yarn vercel --prebuilt --prod
