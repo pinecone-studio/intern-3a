ac552b2fdac0ca89642e13710408428ee81c6151
Khatanbaatar A <25lp5598@nest.edu.mn>
2026-01-08 13:29:04 +0800

    registration page

---
+++ apps/skill-hub/src/app/register/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+import React, { use } from 'react';
+
+interface RegistrationPageProps {
+  searchParams: Promise<{
+    clubId?: string | undefined;
+    level?: string | undefined;
+  }>;
+}
+
+const page = ({ searchParams }: RegistrationPageProps) => {
+  const { clubId, level } = use(searchParams);
+  console.log('clubId and level', clubId, level);
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <h1>Registration Page</h1>
+      <p>
+        CLUB ID: <span className="font-mono">{clubId}</span>
+      </p>
+      <p>
+        LEVEL: <span className="font-mono">{level}</span>
+      </p>
+    </div>
+  );
+};
+
+export default page;
