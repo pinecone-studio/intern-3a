4580ad7e4315e9fa7691b4a8bca297b85e17f70b
Naransolongo <nnaso0275@gmail.com>
2026-01-15 16:43:03 +0800

    my section (#337)

--- apps/referu-employee-fe/src/app/page.tsx
+++ apps/referu-employee-fe/src/app/page.tsx
@@ -1,11 +1,14 @@
+'use client';
 import { Badge, Button } from '@intern-3a/shadcn';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const HomePage = () => {
+  const router = useRouter();
   return (
     <div className="w-full bg-red-100">
       <Badge></Badge>
-      <Button>Миний</Button>
+      <Button onClick={() => router.push('/mySection')}>Миний</Button>
     </div>
   );
 };
