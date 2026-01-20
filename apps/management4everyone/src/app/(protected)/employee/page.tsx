304ad3635401cb72f3f5c134afbfd8f1a54d9aeb
Khatanbaatar A <hbbaatar@gmail.com>
2026-01-20 15:56:14 +0800

    admin layout

---
+++ apps/management4everyone/src/app/(protected)/employee/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const page = () => {
+  return <div>Employee's page</div>;
+};
+
+export default page;
--- apps/management4everyone/src/app/(protected)/layout.tsx
+++ apps/management4everyone/src/app/(protected)/layout.tsx
@@ -1,16 +1,14 @@
 'use client';
 
 import { useUser } from '@clerk/nextjs';
-import { SidebarProvider, SidebarTrigger } from '@intern-3a/shadcn';
+
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { ClerkHeader } from '../_components/ClerkHeader';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const { user, isLoaded } = useUser();
   const router = useRouter();
-
-  const [open, setOpen] = useState<boolean>(false);
 
   useEffect(() => {
     if (!isLoaded) return;
@@ -28,19 +26,8 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <ClerkHeader />
-      <SidebarProvider className="bg-white" open={open} onOpenChange={setOpen}>
-        <main>
-          <div>
-            {!open && (
-              <div className="pt-18 px-4 h-screen border border-[#E4E4E7]">
-                <SidebarTrigger className="w-6 h-6" />
-              </div>
-            )}
-          </div>
-        </main>
 
-        {children}
-      </SidebarProvider>
+      {children}
     </>
   );
 }
