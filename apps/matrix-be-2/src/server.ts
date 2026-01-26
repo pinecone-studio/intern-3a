5c488455d809f174881ffdf1c478188ab3b53f4f
Batt866 <25LP6030@nest.edu.mn>
2026-01-26 14:58:34 +0800

    Save changes in install-state.gz and server.ts

--- apps/matrix-be-2/src/server.ts
+++ apps/matrix-be-2/src/server.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => res.send('Backend is running!'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 // server.ts
 app.get('/exercises', async (_, res) => {
