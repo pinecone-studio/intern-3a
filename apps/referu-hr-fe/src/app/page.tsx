a76b4ecd8f8df87458b217600f4f8dc697d01abd
Khishigdari <nbhishgee22@gmail.com>
2026-01-28 16:34:27 +0800

    clerk connected referu hr fe 1.28

--- apps/referu-hr-fe/src/app/page.tsx
+++ apps/referu-hr-fe/src/app/page.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+"use client"
+
+import { useUser } from '@clerk/nextjs';
+import { Loader } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 import Page from './homepage/page';
 
 const HomePage = () => {
+const {user, isLoaded} = useUser()
+const router =useRouter()
+
+useEffect(()=>{
+  if (!isLoaded) return
+
+  if (!user) {router.push("/sign-in")}
+},[])
+
+    if (!user || !isLoaded )
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <Loader className="animate-spin" size={32} />
+      </div>
+    );
   return (
     <div>
       <Page />
